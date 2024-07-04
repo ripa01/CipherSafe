a8627bffd3698bc286638354e3b4f1209078277a
ripa01 <ripabiswas2739@gmail.com>
2024-07-04 14:53:35 +0600

    design home page UI

--- ciphersafe/src/components/Home.jsx
+++ ciphersafe/src/components/Home.jsx
@@ -1,36 +1,45 @@
 import React from "react";
+import { useRef } from "react";
 
 export default function Home() {
+    const ref = useRef()
+    const showpass = () => {
+        if(ref.current.src.includes("open.svg")){
+        ref.current.src = "close.svg"
+        }else { ref.current.src = "open.svg"}
+        
+
+    }
   return (
     <>
-      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-      <body class="font-poppins w-screen flex flex-col">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <body className="font-poppins w-screen flex flex-col">
   
     <div
         id="body"
-        class=" w-full items-center justify-center"
+        className=" w-full items-center justify-center"
     >
-        <div class="justify-around items-center gap-8 mb-16">
+        <div className="justify-around items-center gap-8 mb-16">
             <div
                 
-                class="h-full flex flex-col justify-center bg-indigo-100 rounded-2xl px-11 py-8 shadow-xl border-2 border-black"
+                className="h-full flex flex-col justify-center bg-indigo-100 rounded-2xl px-11 py-8 shadow-xl border-2 border-black"
             >
                 <form method="POST">
                   
                     <div
-                        class="flex flex-col justify-center items-center gap-4"
+                        className="flex flex-col justify-center items-center gap-4"
                     >
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cipher<span className='text-blue-500 font-thin'>Safe</span></span>
                         
-                        <p class="text-gray-500 text-sm text-center px-8 pb-2">
+                        <p className="text-gray-500 text-sm text-center px-8 pb-2">
                             Protect your password!
                         </p>
                         <div
-                            class="w-4/5 flex flex-row gap-4 justify-center items-center"
+                            className="w-4/5 flex flex-row gap-4 justify-center items-center"
                         >
                             <input
                                 type="text"
-                                class="w-full text-sm h-10 rounded-2xl p-4 bg-blue-50 ring-2 ring-violet-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                                className="w-full text-sm h-10 rounded-2xl p-4 bg-blue-50 ring-2 ring-violet-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
                                 placeholder="Website URL"
                                 name="url"
                                 required
@@ -38,35 +47,43 @@ export default function Home() {
                            
                         </div>
                         <div
-                            class="w-4/5 flex flex-row gap-4 justify-center items-center"
+                            className="w-4/5 flex flex-row gap-4 justify-center items-center"
                         >
                             <input
                                 type="text"
-                                class="w-4/5 text-sm h-10 rounded-2xl p-4 bg-blue-50 ring-2 ring-violet-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                                className="w-2/5 text-sm h-10 rounded-2xl p-4 bg-blue-50 ring-2 ring-violet-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
                                 placeholder="Username"
                                 name="username"
                                 required
                             />
+                            <div className="relative w-2/5">
                                <input
                                 type="password"
-                                class="w-3/4 text-sm h-10 rounded-2xl p-4 bg-blue-50 ring-2 ring-violet-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                                className="w-full text-sm h-10 rounded-2xl p-4 bg-blue-50 ring-2 ring-violet-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
                                 placeholder="Password"
                                 name="password1"
                                 required
+                                
                             />
+                            <span className="absolute right-1 top-1.5 cursor-pointer" onClick={showpass}><img ref={ref} className="p-1" width={30} src='close.svg'/></span>
+                            </div>
                          
                         </div>
                        
                         <div
-                            class="w-4/5 flex flex-row gap-4 justify-center items-center"
+                            className="w-4/5 flex flex-row gap-4 justify-center items-center"
                         >
                          
                         </div>
 
                         <button
                             type="submit"
-                            class="bg-blue-600 mt-1 h-12 w-2/4 rounded-2xl text-primary font-bold hover:bg-primary hover:text-white border-primary border-2"
+                            className="bg-blue-600 flex justify-center items-center mt-1 h-12 w-1/4 rounded-2xl text-primary font-bold hover:bg-primary hover:bg-blue-400 border-primary border-2"
+                        > <lord-icon
+                        src="https://cdn.lordicon.com/jgnvfzqg.json"
+                        trigger="hover"
                         >
+                    </lord-icon>
                             Save
                         </button>
                    
