c4f9a1e9867803c8bb7445c818e686c012895744
ripa01 <ripabiswas2739@gmail.com>
2024-07-04 20:38:37 +0600

    display password from local storage

--- ciphersafe/src/components/Home.jsx
+++ ciphersafe/src/components/Home.jsx
@@ -113,6 +113,58 @@ export default function Home() {
                 </div>
               </form>
             </div>
+
+            <div>
+
+
+
+<div class="flex justify-center items-center overflow-x-auto shadow-md sm:rounded-lg">
+  {passwordArray.length === 0 && <div className="text-white">No passwords to show </div>}
+  {passwordArray.length != 0 &&  <table class="w-4/5 text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-2xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Website name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                   User Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Password
+                </th>
+              
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+          {passwordArray.map((item)=>{
+           return <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <a href={item.site} target="_blank">{item.site}</a>
+            </th>
+            <td class="px-6 py-4">
+            {item.username}
+            </td>
+            <td class="px-6 py-4">
+            {item.pass}
+            </td>
+            
+            <td class="px-6 py-4">
+                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+            </td>
+        </tr>
+
+          })}
+            
+            
+            
+        </tbody>
+    </table>}
+</div>
+
+            </div>
           </div>
         </div>
       </div>
